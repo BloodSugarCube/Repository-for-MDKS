7b04d506fe951cce199b5c4164ad33a3aaedeea9
Aleksandr Aganin <bscaaa@mail.ru>
2022-11-22 03:50:23 +0300

    feat(lw2-3):simple-app

--- lw1/resources/views/home.blade.php
+++ lw1/resources/views/home.blade.php
@@ -25,7 +25,7 @@
                 <div class="info-bloks__element">
                     <div class="element-nameicon">
                     <img src=" {{ asset('img/discount.svg') }}" class="element-icon">
-                    <h2>Incididunt</h2>
+                    <h2></h2>
                     </div>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
                 </div>
